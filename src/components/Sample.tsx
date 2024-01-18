ac559292d4392a713422c85bc2d496f299262e33
JohannesXerxes <johannes.dalogdog@supportzebra.com>
2024-01-18 08:12:37 +0800

    changes

---
+++ src/components/Sample.tsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+function Sample() {
+  return (
+    <div>
+      <div css={css({
+        margin: 10,
+        padding: 10,
+        backgroundColor: '#eee',
+      })}>
+        This is an example of <code>`css`</code> using an object.
+      </div>
+      <div css={css`
+        margin: 10px;
+        padding: 10px;
+        background-color: #eee;
+      `}>
+        This is an example of <code>`css`</code> using a tagged template literal.
+      </div>
+    </div>
+  );
+}
+
+export default Sample;
