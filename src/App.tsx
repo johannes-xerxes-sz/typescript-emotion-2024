b0eae5c449975c749f465d884b96e31e4a599b4e
johannes-xerxes-sz <johannes.dalogdog@supportzebra.com>
2024-01-18 10:31:32 +0800

    set up emotion successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-/** @jsxImportSource @emotion/react */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { css } from '@emotion/react'
+import { css } from "@emotion/react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
- //test
+  const [count, setCount] = useState(0);
+  //test
   return (
     <>
       <div>
@@ -30,24 +29,29 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-          <div>
-      <div css={css({
-        margin: 10,
-        padding: 10,
-        backgroundColor: '#eee',
-      })}>
-        This is an example of <code>`css`</code> using an object.
+      <div>
+        <div
+          css={css({
+            margin: 10,
+            padding: 10,
+            backgroundColor: "blue",
+          })}
+        >
+          This is an example of <code>`css`</code> using an object.
+        </div>
+        <div
+          css={css`
+            margin: 10px;
+            padding: 10px;
+            background-color: #eee;
+          `}
+        >
+          This is an example of <code>`css`</code> using a tagged template
+          literal.
+        </div>
       </div>
-      <div css={css`
-        margin: 10px;
-        padding: 10px;
-        background-color: #eee;
-      `}>
-        This is an example of <code>`css`</code> using a tagged template literal.
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
